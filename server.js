871bc235893c373533af9f2507d2ed5379295815
KEVP <141309394+kev-platforms@users.noreply.github.com>
2026-07-11 09:15:27 -0400

    Update route handling for index.html

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/api/publicaciones/:id/valorar', (req, res) => {
 });
 
 // Enrutar cualquier otra petición al index.html
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
